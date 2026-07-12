f6e033cd67046974bbc62ed9e6fc5504c1a4b82e
Teplykh Semyon <teplihsemen@gmail.com>
2026-07-12 21:39:04 +0500

    Первый коммит: структура проекта + библиотека getopt + оформление

---
+++ app/src/main.c
@@ -0,0 +1,14 @@
+/*
+main.c - главный модуль программы. 
+
+Теплых Семён Сергеевич
+МК-101
+*/
+
+#include <stdio.h>
+#include "lib_main.h"
+
+int main(int argc, char** argv) {
+    // логика
+    return 0;
+}
---
+++ lib/include/lib_main.h
@@ -0,0 +1,13 @@
+/*
+lib_main.h - заголовочный файл модуля lib_main.c. 
+
+Теплых Семён Сергеевич
+МК-101
+*/
+
+#ifndef LIB_MAIN_H
+#define LIB_MAIN_H
+
+// функции
+
+#endif // LIB_MAIN_H
